165b0083ceae2e0ef3fc3bca7236974e0b8c2a2a
CharismakProject <charismakprojectnigltd@gmail.com>
2026-08-21 20:12:48 +0100

    Lock security-definer RPCs from anonymous access

---
+++ supabase/migrations/20260821201500_revoke_public_anonymous_security_definer_execution_v2.sql
@@ -0,0 +1,12 @@
+do $$
+declare r record;
+begin
+  for r in
+    select n.nspname, p.proname, pg_get_function_identity_arguments(p.oid) as args
+    from pg_proc p
+    join pg_namespace n on n.oid=p.pronamespace
+    where n.nspname='public' and p.prosecdef
+  loop
+    execute format('revoke execute on function %I.%I(%s) from public, anon', r.nspname, r.proname, r.args);
+  end loop;
+end $$;
