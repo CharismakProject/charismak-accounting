aaf31141366a0cfd23f731958121268a461a8c9b
CharismakProject <charismakprojectnigltd@gmail.com>
2026-08-21 05:27:38 +0100

    Load final mobile web overrides last

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import "./product-simplification.css";
 import "./branding.css";
 import "./reports.css";
 import "./typography-system.css";
+import "./web-mobile-final.css";
 import PwaRegister from "./PwaRegister";
 import NotificationBell from "./NotificationBell";
 import MobileMenu from "./MobileMenu";
